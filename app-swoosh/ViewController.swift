79f8b1e1e237175e5dd4fd080bf9fbff941370ce
berarduc <tom.berarducci@gmail.com>
2019-01-15 16:35:34 -0800

    added seques

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
 
 
 }
